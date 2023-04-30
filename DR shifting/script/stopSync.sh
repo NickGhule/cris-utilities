65e43c27f8f2054e29196abfc9b8cd309513dc04
NickGhule <nikghule1@gmail.com>
2023-04-30 17:23:03 +0530

    Fix: SSH hang and blank output of pgrep
    Modified as per local server

--- DR shifting/script/stopSync.sh
+++ DR shifting/script/stopSync.sh
@@ -5,40 +5,42 @@ mainPath='/home/nickghule/data/cris/DR shifting'
 source "$mainPath"/resource/globals.sh
 
 
-pmuutmls03="10.129.1.150"
-pmuutmls04="10.129.1.151"
+pmuutmls03="10.0.100.129"
+pmuutmls04="10.0.100.129"
 
 # Stop ASA 12 sync
 # Instance 1 on pmuutmls03
-ssh relcrml12@"$pmuutmls03" "bash -s /mobiscript/asa12/uts/sh/stop'$1'ml1"
+ssh -n relcrml12@"$pmuutmls03" "bash /mobiscript/asa12/uts/sh/stop""$1""ml1"
 echo -en "Stopping Sync (ML 12 Instance 1) :"
 loadingAnimation 4
 
 # check if process with dsn_ml12_pucrut_db is running on pmuutmls03
-if ssh relcrml12@"$pmuutmls03" "pgrep dsn_ml12_pucrut_db" > /dev/null
+if ssh relcrml12@"$pmuutmls03" "pgrep -fc dsn_ml12_pucrut_db1" > /dev/null
 then
+
     echo -e " $redCross"
     printf "\e[33m%s\e[0m\n" "Ml12 database is still running. Please stop manually."
-    echo -e "Press enter to continue"
+    echo -en "Press enter to continue"
     read -r
-    exit 1
+    printf "\033[2A"
 else
     echo -e " $greenCheck"
 fi
 
 # instance 2 on pmuutmls04
-ssh relcrml12@"$pmuutmls04" "bash -s /mobiscript/asa12/uts/sh/stop'$1'ml2"
+ssh relcrml12@"$pmuutmls04" "bash /mobiscript/asa12/uts/sh/stop'$1'ml2"
 echo -en "Stopping Sync (ML 12 Instance 2) :"
 loadingAnimation 4
 
 # check if process with dsn_ml12_pucrut_db is running on pmuutmls04
-if ssh relcrml12@"$pmuutmls04" "pgrep dsn_ml12_pucrut_db" > /dev/null
+if ssh relcrml12@"$pmuutmls04" "pgrep -fc dsn_ml12_pucrut_db2" > /dev/null
 then
     echo -e " $redCross"
     printf "\e[33m%s\e[0m\n" "Ml12 database is still running. Please stop manually."
-    echo -e "Press enter to continue"
+    echo -en "Press enter to continue"
     read -r
-    exit 1
+    # delete last two lines
+    printf "\033[2A"
 else
     echo -e " $greenCheck"
 fi
@@ -46,35 +48,35 @@ fi
 
 # Stop ASA 17 sync
 # Instance 1 on pmuutmls03
-ssh relcrml17@"$pmuutmls03" "bash -s /mobiscript/asa17/uts/sh/stop'$1'ml1"
+ssh relcrml17@"$pmuutmls03" "bash /mobiscript/asa17/uts/sh/stop'$1'ml1"
 echo -en "Stopping Sync (ML 17 Instance 1) :"
 loadingAnimation 4
 
 # check if process with dsn_ml17_pucrut_db is running on pmuutmls03
-if ssh relcrml17@"$pmuutmls03" "pgrep dsn_ml17_pucrut_db" > /dev/null
+if ssh relcrml17@"$pmuutmls03" "pgrep -fc dsn_ml17_pucrut_db1" > /dev/null
 then
     echo -e " $redCross"
     printf "\e[33m%s\e[0m\n" "Ml17 database is still running. Please stop manually."
-    echo -e "Press enter to continue"
+    echo -en "Press enter to continue"
     read -r
-    exit 1
+    printf "\033[2A"
 else
     echo -e " $greenCheck"
 fi
 
 # instance 2 on pmuutmls04
-ssh relcrml17@"$pmuutmls04" "bash -s /mobiscript/asa17/uts/sh/stop'$1'ml2"
+ssh relcrml17@"$pmuutmls04" "bash /mobiscript/asa17/uts/sh/stop'$1'ml2"
 echo -en "Stopping Sync (ML 17 Instance 2) :"
 loadingAnimation 4
 
 # check if process with dsn_ml17_pucrut_db is running on pmuutmls04
-if ssh relcrml17@"$pmuutmls04" "pgrep dsn_ml17_pucrut_db" > /dev/null
+if ssh relcrml17@"$pmuutmls04" "pgrep -fc dsn_ml17_pucrut_db2" > /dev/null
 then
     echo -e " $redCross"
     printf "\e[33m%s\e[0m\n" "Ml17 database is still running. Please stop manually."
-    echo -e "Press enter to continue"
+    echo -en "Press enter to continue"
     read -r
-    exit 1
+    printf "\033[2A"
 else
     echo -e " $greenCheck"
 fi
