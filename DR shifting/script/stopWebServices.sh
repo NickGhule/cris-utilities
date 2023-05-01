24284fcb38b456ec78018ce18993f3216d8eec5b
NickGhule <nikghule1@gmail.com>
2023-05-01 21:02:50 +0530

    Docs: renamed stopWebService.sh to stopWebServices.sh

--- DR shifting/script/stopWebServices.sh
+++ DR shifting/script/stopWebServices.sh
@@ -8,8 +8,8 @@ source "$mainPath"/resource/globals.sh
 if [ "$1" = "CR" ]
 then 
     echo "CR"
-    web1="10.129.1.143"
-    web2="10.129.1.144"
+    web1="10.0.100.129"
+    web2="10.0.100.129"
 elif [ "$1" = "WR" ]
 then
     echo "WR"
@@ -26,9 +26,9 @@ fi
 
 # Stop web Instance 1
 echo -en "Stopping web instance 1 :"
-ssh utswebsrv@"$web1" "sudo systemctl stop tomcat@ws_uts_'$1'_web"
+ssh utswebsrv@"$web1" "sudo systemctl start tomcat"
 loadingAnimation 4
-if ssh utswebsrv@"$web1" "sudo systemctl status tomcat@ws_uts_'$1'_web"| grep -q "Active: inactive (dead)"
+if ssh utswebsrv@"$web1" "sudo systemctl status tomcat"| grep -q "Active: inactive (dead)"
 then
     echo -e " $greenCheck"
 else
@@ -36,20 +36,24 @@ else
     printf "\e[33m%s\e[0m\n" "Error in stopping web instance 1. Please check manually."
     echo -e "Press enter to continue"
     read -r
-    exit 1
+    printf "\033[1A\033[0K"
+    printf "\033[1A\033[0K"
+    printf "\033[1A\033[0K"
 fi
 
 # Stop web Instance 2
 echo -en "Stopping web instance 2 :"
-ssh utswebsrv@"$web2" "sudo systemctl stop tomcat@ws_uts_'$1'_web"
+ssh utswebsrv@"$web2" "sudo systemctl stop tomcat"
 loadingAnimation 4
-if ssh utswebsrv@"$web2" "sudo systemctl status tomcat@ws_uts_'$1'_web"| grep -q "Active: inactive (dead)"
+if ssh utswebsrv@"$web2" "sudo systemctl status tomcat"| grep -q "Active: inactive (dead)"
 then
     echo -e " $greenCheck"
 else
     echo -e " $redCross"
     printf "\e[33m%s\e[0m\n" "Error in stopping web instance 2. Please check manually."
-    echo -e "Press enter to continue"
+    echo -en "Press enter to continue"
     read -r
-    exit 1
+    printf "\033[1A\033[0K"
+    printf "\033[1A\033[0K"
+    printf "\033[1A\033[0K"
 fi
